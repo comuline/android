1dd5feb958a5c86714f5e5e53bd62da9d7ffad8d
ardasatata <ardasatata@gmail.com>
2025-04-20 21:55:35 +0900

    feat: add google font

--- app/src/main/java/com/comuline/app/ui/theme/Type.kt
+++ app/src/main/java/com/comuline/app/ui/theme/Type.kt
@@ -2,14 +2,53 @@ package com.comuline.app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.comuline.app.R
+
+private val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Roboto")
+
+val fontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider)
+)
+
+val fontNameMono = GoogleFont("Roboto Mono", true)
+
+val fontFamilyMono = FontFamily(
+    Font(googleFont = fontNameMono, fontProvider = provider)
+)
+
 
 val Typography = Typography(
+    bodyMedium = TextStyle(
+        fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 12.sp/*...*/
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 2.sp,
+        /*...*/
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = fontFamily, fontWeight = FontWeight.SemiBold/*...*/
+    ),
     bodySmall = TextStyle(
-        fontFamily = FontFamily.Monospace,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = fontFamilyMono,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
     ),
 )

9a673ce49eac1fb5cf71eafeda505b46ece4ad74
ardasatata <ardasatata@gmail.com>
2025-04-29 20:54:20 +0900

    feat: added `stations`

--- app/src/main/java/com/comuline/app/repository/WatchedStationRepository.kt
+++ app/src/main/java/com/comuline/app/repository/WatchedStationRepository.kt
@@ -1,7 +1,6 @@
 package com.comuline.app.repository
 
 import com.comuline.app.database.AppDatabase
-import com.comuline.app.database.SavedStationEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import timber.log.Timber
@@ -10,23 +9,23 @@ import javax.inject.Inject
 class WatchedStationRepository @Inject constructor(
     private val appDatabase: AppDatabase
 ) {
-    val stations: Flow<List<SavedStationEntity>?> =
-        appDatabase.schedulesDao.getWatchedStations().map { it }
-
-
-    fun saveStation(id: String) {
-        try {
-            appDatabase.schedulesDao.insertWatchedStation(SavedStationEntity(id))
-        } catch (e: Exception) {
-            Timber.w(e)
-        }
-    }
-
-    fun removeStation(id: String) {
-        try {
-            appDatabase.schedulesDao.deleteWatchedStation(SavedStationEntity(id))
-        } catch (e: Exception) {
-            Timber.w(e)
-        }
-    }
+//    val stations: Flow<List<SavedStationEntity>?> =
+//        appDatabase.schedulesDao.getWatchedStations().map { it }
+//
+//
+//    fun saveStation(id: String) {
+//        try {
+//            appDatabase.schedulesDao.insertWatchedStation(SavedStationEntity(id))
+//        } catch (e: Exception) {
+//            Timber.w(e)
+//        }
+//    }
+//
+//    fun removeStation(id: String) {
+//        try {
+//            appDatabase.schedulesDao.deleteWatchedStation(SavedStationEntity(id))
+//        } catch (e: Exception) {
+//            Timber.w(e)
+//        }
+//    }
 }

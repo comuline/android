1b0a7ebc111b655b82f7ccd23c66e8a4424f3efc
ardasatata <ardasatata@gmail.com>
2025-04-29 20:54:47 +0900

    feat: add `schedule`

--- app/src/main/java/com/comuline/app/network/model/ScheduleApiModel.kt
+++ app/src/main/java/com/comuline/app/network/model/ScheduleApiModel.kt
@@ -1,54 +1,86 @@
 package com.comuline.app.network.model
 
+import com.comuline.app.database.OriginColorEntity
+import com.comuline.app.database.OriginEntity
 import com.comuline.app.database.ScheduleEntity
+import com.comuline.app.database.ScheduleMetadataEntity
+import com.comuline.app.database.StationEntity
+import com.comuline.app.database.StationMetadataEntity
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class ScheduleData (
+data class ScheduleResponse(
     @Json(name = "id")
     val id: String,
-    @Json(name = "stationId")
-    val stationId: String = "",
-    @Json(name = "trainId")
-    val trainId: String = "",
+
+    @Json(name = "station_id")
+    val stationId: String,
+
+    @Json(name = "station_origin_id")
+    val stationOriginId: String,
+
+    @Json(name = "station_destination_id")
+    val stationDestinationId: String,
+
+    @Json(name = "train_id")
+    val trainId: String,
+
     @Json(name = "line")
-    val line: String = "",
+    val line: String,
+
     @Json(name = "route")
-    val route: String = "",
-    @Json(name = "color")
-    val color: String = "",
-    @Json(name = "destination")
-    val destination: String = "",
-    @Json(name = "timeEstimated")
-    val timeEstimated: String = "",
-    @Json(name = "destinationTime")
-    val destinationTime: String = "",
-    @Json(name = "updatedAt")
-    val updatedAt: String = "",
+    val route: String,
+
+    @Json(name = "departs_at")
+    val departsAt: String,
+
+    @Json(name = "arrives_at")
+    val arrivesAt: String,
+
+    @Json(name = "metadata")
+    val metadata: ScheduleMetadata,
+
+    @Json(name = "created_at")
+    val createdAt: String,
+
+    @Json(name = "updated_at")
+    val updatedAt: String
 )
 
 @JsonClass(generateAdapter = true)
-data class ScheduleApiModel (
-    @Json(name = "status")
-    val status: Int,
-    @Json(name = "data")
-    val data: List<ScheduleData>,
+data class ScheduleMetadata(
+    @Json(name = "origin")
+    val origin: OriginColor? = null
 )
 
-fun ScheduleApiModel.asDomainModel(): List<ScheduleEntity> {
-    return data.map {
-        ScheduleEntity(
-            id = it.id,
-            stationId = it.stationId,
-            trainId = it.trainId,
-            line = it.line,
-            route = it.route,
-            color = it.color,
-            destination = it.destination,
-            timeEstimated = it.timeEstimated,
-            destinationTime = it.destinationTime,
-            updatedAt = it.updatedAt
-        )
+@JsonClass(generateAdapter = true)
+data class OriginColor(
+    @Json(name = "color")
+    val color: String? = null
+)
+
+fun ScheduleResponse.toEntity() = ScheduleEntity(
+    id = id,
+    stationId = stationId,
+    stationOriginId = stationOriginId,
+    stationDestinationId = stationDestinationId,
+    trainId = trainId,
+    line = line,
+    route = route,
+    departsAt = departsAt,
+    arrivesAt = arrivesAt,
+    metadata = ScheduleMetadataEntity(
+        origin = metadata.origin?.let {
+            OriginColorEntity(color = it.color)
+        }
+    ),
+    createdAt = createdAt,
+    updatedAt = updatedAt
+)
+
+fun List<ScheduleResponse>.toEntity() : List<ScheduleEntity>{
+    return map {
+        it.toEntity()
     }
 }
--- app/src/main/java/com/comuline/app/repository/ScheduleRepository.kt
+++ app/src/main/java/com/comuline/app/repository/ScheduleRepository.kt
@@ -3,26 +3,33 @@ package com.comuline.app.repository
 import com.comuline.app.database.AppDatabase
 import com.comuline.app.database.asDomainModel
 import com.comuline.app.domain.Schedule
-import com.comuline.app.network.ScheduleApi
-import com.comuline.app.network.model.asDomainModel
+import com.comuline.app.domain.Station
+import com.comuline.app.network.ComulineApi
+import com.comuline.app.network.model.toEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import timber.log.Timber
 import javax.inject.Inject
 
 class ScheduleRepository @Inject constructor(
-    private val scheduleApi: ScheduleApi,
+    private val comulineApi: ComulineApi,
     private val appDatabase: AppDatabase
 ) {
     val schedules: Flow<List<Schedule>?> =
-        appDatabase.schedulesDao.getAllSchedules().map { it?.asDomainModel() }
+        appDatabase.comulineDao.getAllSchedules().map { it?.asDomainModel() }
 
     suspend fun getScheduleByStationId(stationId: String) {
         try {
-            val schedules = scheduleApi.getSchedule(stationId, "true")
-            appDatabase.schedulesDao.insertSchedules(schedules.asDomainModel())
+            val schedules = comulineApi.getSchedule(stationId)
+            appDatabase.comulineDao.insertSchedules(schedules.data.toEntity())
         } catch (e: Exception) {
             Timber.w(e)
         }
     }
+
+//    suspend fun cleanupPastSchedules() {
+//        val currentTime = getCurrentTimeAsString()
+//        println("currentTime: $currentTime")
+////        appDatabase.schedulesDao.deleteSchedulesWithPastDepartureTime(currentTime)
+//    }
 }
